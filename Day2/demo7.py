5dc22ef771be5748d89746181454bff3c064cec3
Vishwas K Singh <vishwasks32@gmail.com>
2025-07-31 15:06:06 +0530

    code updates

--- Day2/demo7.py
+++ Day2/demo7.py
@@ -6,7 +6,53 @@ def house_data_loader(file_path):
         for row in reader:
             yield row
 
-generator = house_data_loader("./data/house_rent.csv")
 
-for _ in range(5):
-    print(next(generator))
+def city_filter(city_name, target):
+    '''
+        Based on the city name, gives us particular record
+    '''
+    try:
+        while True:
+            row = (yield)
+            if row["City"] == city_name:
+                target.send(row)
+    except GeneratorExit:
+        target.close()
+
+def transform_data(target):
+    try:
+        while True:
+            row = (yield)
+            row["Rent"] = int(row["Rent"])
+            # Size sqft -> sqm
+            row["Size"] = float(row["Size"]) * 0.092903
+            target.send(row)
+    except GeneratorExit:
+        target.close()
+
+def data_sink():
+    try:
+        while True:
+            row = (yield)
+            print("Processed Row:", row)
+    except GeneratorExit:
+        print("Done Streaming....")
+
+sink = data_sink()
+next(sink)
+
+transform = transform_data(sink)
+next(transform)
+
+filter_corout = city_filter("Kolkata", transform)
+next(filter_corout)
+
+for row in house_data_loader("./data/house_rent.csv"):
+    filter_corout.send(row)
+
+filter_corout.close()
+
+# generator = house_data_loader("./data/house_rent.csv")
+
+# for _ in range(5):
+#     print(next(generator))
