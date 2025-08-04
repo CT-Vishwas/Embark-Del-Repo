1a47f79761ada7f2e10d324ded76ff66d0d2ea6c
Vishwas K Singh <vishwasks32@gmail.com>
2025-08-04 16:35:39 +0530

    django codes

---
+++ Day4/djapp/todo/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+# Create your views here.
+def index(request):
+    tasks = Task.objects.all()
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'todo/index.html', context)
